53a32a6bb3cdae18420b0cf351508df80849f4c3
Diego <diego.nicolas.gandara@gmail.com>
2021-08-10 23:18:53 -0300

    Ahora, llamamos a esa funcion -getUserInformation- desde el contest de AuthContext y pedimos que pase el nombre que viene ya decodificado.

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,14 @@
-
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
 
 const Dashboard = () => {
-    return (
-        <h2>Dashboard</h2>
-     );
-}
+  const { getUserInformation } = useContext(AuthContext);
+  return (
+    <>
+      <h2>Dashboard</h2>
+      {getUserInformation().name}
+    </>
+  );
+};
 
 export default Dashboard;
